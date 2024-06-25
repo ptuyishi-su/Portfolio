07f74cdd08adcc63c11d053eb97b09db46bf2c60
patrickT <patricktuyishime@Patricks-MacBook-Air.local>
2024-06-25 14:03:12 +0100

    test website

--- src/Pages/Home/MyPortfolio.jsx
+++ src/Pages/Home/MyPortfolio.jsx
@@ -1,5 +1,6 @@
 import useContentful from "../../data/useContentful";
 import { useEffect, useState } from "react";
+import ProjectCard from "./ProjectCard";
 
 export default function MyPortfolio({ projects }) {
     const [authors, setAuthors] = useState([]);
@@ -10,15 +11,17 @@ export default function MyPortfolio({ projects }) {
     }, [getAuthors]);
 
     return (
-        <section className="portfolio--section" id="myPortfolio">
-            <div className="font-extrabold">
-                <h1 className="text-white">Recent Work &gt;</h1>
+        <section class=" overflow-visible" id="myPortfolio">
+            <h1 class=" text-white tracking-wider ">Recent Work&gt;</h1>
+            <div class="flex-row-reverse  lg:flex gap-5 pb-10">
                 {authors.map((author, index) => (
-                    <div key={index}>
+                    <div key={index} class=" bg-neutral-900 rounded-t-3xl">
                         {author.thumbnail && <img src={author.thumbnail} alt={author.title} />}
-                        <h2>{author.title}</h2>
-                        <p>{author.description}</p>
-                        {/* Add more fields as needed */}
+                        <div class="px-8">
+                            <p class=" text-lg">{author.title}</p>
+                            <p>{author.description}</p>
+                            {/* Add more fields as needed */}
+                        </div>
                     </div>
                 ))}
             </div>
