5bd7d1ce7e2b71d6d38ca1e202ee95afee98c8a0
patrickT <patricktuyishime@Patricks-MacBook-Air.local>
2024-07-04 11:03:07 -0400

    dynamic routing and font changes

--- src/App.js
+++ src/App.js
@@ -1,35 +1,33 @@
-
+// src/App.js
 import "./App.css";
 import React from "react";
-import { BrowserRouter as Router, Route, Routes, BrowserRouter } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import NavBar from "./Pages/Home/NavBar";
 import Footer from "./Pages/Home/Footer";
 import Home from "./Pages/Home/Homescreen";
 import ContactPage from "./Pages/Home/Homescreen/ContactPage";
 import Work from "./Pages/Home/Homescreen/Work";
 import About from "./Pages/Home/Homescreen/About";
-import { Analytics } from "@vercel/analytics/react"
-
+import { Analytics } from "@vercel/analytics/react";
+import ProjectPage from "./Pages/Home/[slug]";
 
 function App() {
   return (
     <div className="App">
-        <Router>
-          <div>
-            <NavBar />
-            <Routes>
-              <Route index element={<Home />}></Route>
-              <Route path="/Home" element={<Home />}></Route>
-              <Route path="/ContactPage" element={<ContactPage />}></Route>
-              <Route path="/Work" element={<Work />}></Route>
-              <Route path="/About" element={<About />}></Route>
-              <Route path="" element={<div>404 Not Found</div>}></Route>
-            </Routes>
-            <Footer />
-          </div>
-        </Router>
-        <Analytics />
-
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="/Home" element={<Home />} />
+          <Route path="/ContactPage" element={<ContactPage />} />
+          <Route path="/Work" element={<Work />} />
+          <Route path="/About" element={<About />} />
+          <Route path="/work/:slug" element={<ProjectPage />} />
+          <Route path="*" element={<div>404 Not Found</div>} />
+        </Routes>
+        <Footer />
+      </Router>
+      <Analytics />
     </div>
   );
 }
